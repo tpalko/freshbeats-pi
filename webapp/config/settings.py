74a5183994fa3285e858cf08cb12482cdbfa327d
Tim Palko <tim@palkosoftware.com>
2016-10-10 00:41:16 -0400

    removed unused? template dir

--- webapp/config/settings.py
+++ webapp/config/settings.py
@@ -25,7 +25,7 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, '..', 'beater/templates'),
+    #os.path.join(BASE_DIR, '..', 'beater/templates'),
 )
 
 ALLOWED_HOSTS = []
