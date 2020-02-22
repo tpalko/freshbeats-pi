308089fa6572916bef707c3ce6d0198e763d6ef4
Tim Palko <tim@palkosoftware.com>
2020-02-22 03:39:40 -0500

    removed deploy folder, obsolete

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-pushd $(dirname $0)
-scp services/beatplayer/mpplayer.py pi@frankenpi: && \
-  ssh pi@frankenpi "sudo rm -fv /opt/freshbeats/beatplayer/mpplayer.py && sudo mv -unv ~/mpplayer.py /opt/freshbeats/beatplayer && sudo systemctl daemon-reload && sudo systemctl restart beatplayer"
-popd
